7679d43fd62edb7d49fc02015bdd4767ad87cf4c
Alex Lazcano <alazcano2000@gmail.com>
2021-02-16 23:17:44 -0800

    Created Splashscreen Component

---
+++ Components/SlashScreen/index.js
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import styles from "./styles";
+
+const SplashScreen = (props) => {
+  return (
+    <View style={styles.splashBG}>
+      <Image source={require("../../assets/LogoPT.png")} style={styles.logo} />
+    </View>
+  );
+};
+export default SplashScreen;
---
+++ Components/SlashScreen/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 500,
+    height: 200,
+    resizeMode: "contain",
+  },
+  splashBG: {
+    backgroundColor: "pink",
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+});
+
+export default styles;
