f21c7494e1e713c47c1817d2b165a5b9dfd5ed49
Alex Lazcano <alazcano2000@gmail.com>
2021-02-19 13:02:09 -0800

    added navigation dependencies and added Navitation container

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import SplashScreen from "./Components/SlashScreen";
+import SplashScreen from "./Components/SlashScreen/SplashScreen";
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <SplashScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={SplashScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     // <View style={styles.container}>
     //   <SplashScreen />
     //   <Text>Open up App.js to start working on your app!</Text>
