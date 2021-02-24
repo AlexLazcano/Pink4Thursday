d0b4db5e57ee44ff0dd4475bf1d1e1eb9561a14e
Alex Lazcano <alazcano2000@gmail.com>
2021-02-23 16:22:48 -0800

    centered landing component

--- Components/Auth/Landing/Landing.js
+++ Components/Auth/Landing/Landing.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
 
+import styles from "./styles";
+
 const Landing = ({ navigation }) => {
   return (
-    <View>
+    <View style={styles.center}>
       <Text>Welcome</Text>
       <Button title="Login" onPress={() => navigation.navigate("Splash")} />
+      <Button title="Sign Up"> </Button>
     </View>
   );
 };
