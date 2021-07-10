21cd3efaed08ba66e607829f12589994d6ebabd9
Alex Lazcano <alazcano2000@gmail.com>
2021-07-10 15:42:56 -0700

    Refactored the Profile Component with styled-components

--- app/src/components/ProfileComponent/ProfileComponent.js
+++ app/src/components/ProfileComponent/ProfileComponent.js
@@ -1,37 +1,24 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
-import GlobalStyles from "../GlobalStyles";
-import styles from "./styles";
+import React from 'react'
+import GlobalStyles from '../GlobalStyles'
+import { ProfileContainer, ProfilePicture, ProTag, Name, Email, Bio } from './styles'
 
-const ProfileComponent = ({isPro}) => {
+const ProfileComponent = ({ isPro }) => {
   return (
-    <View style={[GlobalStyles.PrimaryBG, styles.profileContainer]}>
-      
-        <Image
-          style={styles.profilePicture}
-          source={require("../../../assets/alex.jpg")}
-        />
+    <ProfileContainer style={[GlobalStyles.PrimaryBG]}>
+      <ProfilePicture source={require('../../../assets/alex.jpg')} />
 
-        <Text style={isPro ? styles.isPro : {display: 'none'}}>S|Pro</Text>
-        <Text style={styles.name}>Alex Lazcano</Text>
-        <Text style={styles.email}>alex.lazcano@gmail.com</Text>
+      {isPro && <ProTag>S|Pro</ProTag>}
+      <Name>Alex Lazcano</Name>
+      <Email>alex.lazcano@gmail.com</Email>
 
-        <Text style={styles.bio}>
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-          sint. Velit officia consequat duis enim velit mollit. Exercitation
-          veniam consequat sunt nostrud amet.
-        </Text>
-      
-    </View>
-  );
-};
-
-export default ProfileComponent;
-
-{
-  /* <Text style={styles.isPro}>S|Pro</Text>
-<Text style={styles.name}>Alex Lazcano</Text>
-<Text style={styles.email}>alex.lazcano@gmail.com</Text>
-
-<Text style={styles.bio}>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</Text> */
+      <Bio>
+        hello minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint. Velit officia consequat duis enim velit mollit. Exercitation
+        veniam consequat sunt nostrud amet.
+      </Bio>
+    </ProfileContainer>
+  )
 }
+
+export default ProfileComponent
+
